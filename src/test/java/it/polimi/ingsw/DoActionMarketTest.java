e50ddd77420464435aa4efd625c0ab7fa0c26ee2
bagna <10662176@polimi.it>
2021-04-03 17:59:58 +0200

    commit new correction about index bound problem of deck and a test about the funcionality of the previous commit mechanism new test for More type of cardProduction o developmentBoard

--- src/test/java/it/polimi/ingsw/DoActionMarketTest.java
+++ src/test/java/it/polimi/ingsw/DoActionMarketTest.java
@@ -48,4 +48,103 @@ class DoActionMarketTest {
         }
 
 
+    }
+
+
+
+
+
+
+    /**
+     * test about the reception of cardproduction in gameboard with more deck
+     */
+    @Test
+    public void correctionOfCallMoreDeck() throws LevelException, EmptyException {
+        Game game= new Game();
+
+        Game.DoActionMarket(game.player,game.deckProductionCardOneBlu,1);
+        assertTrue(game.player.getGameBoardOfPlayer().getCellDevelopmentBoard(0,1 ) instanceof ProductionCard);
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardOneBlu,1);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+        Game.DoActionMarket(game.player,game.deckProductionCardTwoBlu,1);
+        Game.DoActionMarket(game.player,game.deckProductionCardThreeBlu,1);
+
+
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardOneBlu,1);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardThreeBlu,1);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+
+
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardTwoBlu,1);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+
+
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardTwoGreen,0);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+
+
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardOneGreen,0);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+        assertTrue(game.player.getGameBoardOfPlayer().getCellDevelopmentBoard(0,0 ).getLevel()==1);
+
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardTwoGreen,0);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+
+        assertTrue(game.player.getGameBoardOfPlayer().getCellDevelopmentBoard(1,0 ).getLevel()==2);
+
+        try {
+            Game.DoActionMarket(game.player,game.deckProductionCardThreeGreen,0);
+        } catch (LevelException e)
+        {
+            System.out.println();
+        }
+
+        assertTrue(game.player.getGameBoardOfPlayer().getCellDevelopmentBoard(2,1 ) .getLevel()==3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }}
