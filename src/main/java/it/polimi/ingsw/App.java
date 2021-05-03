e8d507b8eea9c033599361a8c8d24427d1f42715
bagna <10662176@polimi.it>
2021-05-03 11:31:08 +0200

    :RunTypeAdapterFactory

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import it.polimi.ingsw.server.model.Resource;
+import it.polimi.ingsw.server.model.colours.*;
+import it.polimi.ingsw.server.model.requirements.*;
+
 /**
  * Hello world!
  *
@@ -8,7 +14,27 @@ public class App
 {
     public static void main( String[] args )
     {
+        Colour colourGreen = new Green();
+        Colour colourYellow = new Yellow();
+        Colour colourBlue = new Blue();
+        Colour colourViolet = new Violet();
+        Requirements requirements = new ResourceRequirement(Resource.COIN);
+        Requirements requirements1 = new SecondLevelRequirement(colourBlue);
 
-        System.out.println( "Hello World simo e alice!" );
-    }
-}
+
+
+
+
+        RuntimeTypeAdapterFactory<Requirements> adapter =
+                RuntimeTypeAdapterFactory
+                        .of(Requirements.class)
+                        .registerSubtype(ResourceRequirement.class)
+                        .registerSubtype(SecondLevelRequirement.class)
+                        .registerSubtype(ThreeFlagsTwoColourRequirement.class)
+                        .registerSubtype(TwoFlagsTwoColourRequirement.class);
+
+        Gson gson2=new GsonBuilder().setPrettyPrinting().registerTypeAdapterFactory(adapter).create();
+
+        System.out.println(gson2.toJson(requirements1));
+
+}}
