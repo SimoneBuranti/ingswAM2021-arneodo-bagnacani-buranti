40f184c6000c99cecc760edcd548d6254d98bfba
bagna <10662176@polimi.it>
2021-06-24 12:18:10 +0200

    tests increase coverage

--- src/test/java/it/polimi/ingsw/server/model/GameMultiPlayerTest.java
+++ src/test/java/it/polimi/ingsw/server/model/GameMultiPlayerTest.java
@@ -5,10 +5,11 @@ import it.polimi.ingsw.server.network.ClientHandler;
 import it.polimi.ingsw.server.network.Server;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameMultiPlayerTest {
 
@@ -159,5 +160,261 @@ public class GameMultiPlayerTest {
         FileClass.FileDestroyer();
 
     }
+
+
+
+    @Test
+    public void RestoreTest() throws IOException, InterruptedException {
+        ArrayList<String> nickname = new ArrayList<>();
+        nickname.add("Ali");
+        nickname.add("Simo");
+        nickname.add("Ale");
+        nickname.add("lillo");
+        ArrayList<ClientController> clientControllers = new ArrayList<>(4);
+        Server server= new Server();
+        ClientHandler clientHandler1= new ClientHandler(server);
+        ClientController clientController= new ClientController(server,clientHandler1) ;
+
+        ClientHandler clientHandler2= new ClientHandler(server);
+        ClientController clientController2= new ClientController(server,clientHandler2) ;
+
+        ClientHandler clientHandler3= new ClientHandler(server);
+        ClientController clientController3= new ClientController(server,clientHandler3) ;
+
+        ClientHandler clientHandler4= new ClientHandler(server);
+        ClientController clientController4= new ClientController(server,clientHandler4) ;
+
+        clientControllers.add(clientController);
+        clientControllers.add(clientController2);
+        clientControllers.add(clientController3);
+        clientControllers.add(clientController4);
+
+        clientController.setNickname("simo");
+        clientController2.setNickname("ali");
+        clientController3.setNickname("ale");
+        clientController4.setNickname("lillo");
+        GameMultiPlayer game = new GameMultiPlayer(4, nickname, true, clientControllers);
+
+        try {
+        GameMultiPlayer game1 = new GameMultiPlayer(4, nickname, false, clientControllers);
+        assertEquals(16,game1.leaderDeckSize());
+    }catch(FileNotFoundException e){
+    }
+
+
+
+        FileClass.FileDestroyer();
+
+    }
+
+
+    @Test
+    public void RestoreSingleTest() throws IOException, InterruptedException {
+        ArrayList<String> nickname = new ArrayList<>();
+        nickname.add("simo");
+
+        ArrayList<ClientController> clientControllers = new ArrayList<>(1);
+        Server server= new Server();
+        ClientHandler clientHandler1= new ClientHandler(server);
+        ClientController clientController= new ClientController(server,clientHandler1) ;
+        clientControllers.add(clientController);
+        clientController.setNickname("simo");
+        Game game=new GameSolitaire("simo",true,clientController);
+
+try{
+        Game game2=new GameSolitaire("simo",false,clientController);
+        assertEquals(16,game2.leaderDeckSize());
+    }catch(FileNotFoundException e){
+
+    }
+        FileClass.FileDestroyer();
+
+    }
+
+
+    @Test
+    public void AskTest() throws IOException, InterruptedException {
+        ArrayList<String> nickname = new ArrayList<>();
+        nickname.add("Ali");
+        nickname.add("Simo");
+        nickname.add("Ale");
+        nickname.add("lillo");
+        ArrayList<ClientController> clientControllers = new ArrayList<>(4);
+        Server server= new Server();
+        ClientHandler clientHandler1= new ClientHandler(server);
+        ClientController clientController= new ClientController(server,clientHandler1) ;
+
+        ClientHandler clientHandler2= new ClientHandler(server);
+        ClientController clientController2= new ClientController(server,clientHandler2) ;
+
+        ClientHandler clientHandler3= new ClientHandler(server);
+        ClientController clientController3= new ClientController(server,clientHandler3) ;
+
+        ClientHandler clientHandler4= new ClientHandler(server);
+        ClientController clientController4= new ClientController(server,clientHandler4) ;
+
+        clientControllers.add(clientController);
+        clientControllers.add(clientController2);
+        clientControllers.add(clientController3);
+        clientControllers.add(clientController4);
+
+        clientController.setNickname("simo");
+        clientController2.setNickname("ali");
+        clientController3.setNickname("ale");
+        clientController4.setNickname("lillo");
+        GameMultiPlayer game = new GameMultiPlayer(4, nickname, true, clientControllers);
+        if(game.checkNickname("ali"))
+            game.askInfoOnPlayer(1,"ali");
+        else {
+            game.endOfLastTurn();
+            assertTrue(game.isOver==true);
+        }
+        FileClass.FileDestroyer();
+
+    }
+
+
+    @Test
+    public void AskEndTurnTest() throws IOException, InterruptedException {
+        ArrayList<String> nickname = new ArrayList<>();
+        nickname.add("Ali");
+        nickname.add("Simo");
+        nickname.add("Ale");
+        nickname.add("lillo");
+        ArrayList<ClientController> clientControllers = new ArrayList<>(4);
+        Server server= new Server();
+        ClientHandler clientHandler1= new ClientHandler(server);
+        ClientController clientController= new ClientController(server,clientHandler1) ;
+
+        ClientHandler clientHandler2= new ClientHandler(server);
+        ClientController clientController2= new ClientController(server,clientHandler2) ;
+
+        ClientHandler clientHandler3= new ClientHandler(server);
+        ClientController clientController3= new ClientController(server,clientHandler3) ;
+
+        ClientHandler clientHandler4= new ClientHandler(server);
+        ClientController clientController4= new ClientController(server,clientHandler4) ;
+
+        clientControllers.add(clientController);
+        clientControllers.add(clientController2);
+        clientControllers.add(clientController3);
+        clientControllers.add(clientController4);
+
+        clientController.setNickname("simo");
+        clientController2.setNickname("ali");
+        clientController3.setNickname("ale");
+        clientController4.setNickname("lillo");
+        GameMultiPlayer game = new GameMultiPlayer(4, nickname, true, clientControllers);
+        if(game.checkNickname("ali"))
+            game.askInfoOnPlayer(1,"ali");
+        else
+        {
+            if("lillo".equals(game.currentPlayer.getNickName()))
+            {
+                game.endOfTurn();
+                game.disconnectPlayerOption("lillo");
+                game.disconnectPlayer("lillo");
+                assertTrue(game.numPlayersDisconnected()==1);
+            }
+        }
+
+        FileClass.FileDestroyer();
+
+    }
+
+
+
+    @Test
+    public void AskEndLastTest() throws IOException, InterruptedException {
+        ArrayList<String> nickname = new ArrayList<>();
+        nickname.add("Ali");
+        nickname.add("Simo");
+        nickname.add("Ale");
+        nickname.add("lillo");
+        ArrayList<ClientController> clientControllers = new ArrayList<>(4);
+        Server server= new Server();
+        ClientHandler clientHandler1= new ClientHandler(server);
+        ClientController clientController= new ClientController(server,clientHandler1) ;
+
+        ClientHandler clientHandler2= new ClientHandler(server);
+        ClientController clientController2= new ClientController(server,clientHandler2) ;
+
+        ClientHandler clientHandler3= new ClientHandler(server);
+        ClientController clientController3= new ClientController(server,clientHandler3) ;
+
+        ClientHandler clientHandler4= new ClientHandler(server);
+        ClientController clientController4= new ClientController(server,clientHandler4) ;
+
+        clientControllers.add(clientController);
+        clientControllers.add(clientController2);
+        clientControllers.add(clientController3);
+        clientControllers.add(clientController4);
+
+        clientController.setNickname("simo");
+        clientController2.setNickname("ali");
+        clientController3.setNickname("ale");
+        clientController4.setNickname("lillo");
+        GameMultiPlayer game = new GameMultiPlayer(4, nickname, true, clientControllers);
+        if(game.checkNickname("ali"))
+            game.askInfoOnPlayer(1,"ali");
+        else
+        {
+            game.endOfLastTurn();
+            assertTrue(game.isOver==true);
+        }
+
+        FileClass.FileDestroyer();
+
+    }
+
+
+    @Test
+    public void AskDisconnectTest() throws IOException, InterruptedException {
+        ArrayList<String> nickname = new ArrayList<>();
+        nickname.add("Ali");
+        nickname.add("Simo");
+        nickname.add("Ale");
+        nickname.add("lillo");
+        ArrayList<ClientController> clientControllers = new ArrayList<>(4);
+        Server server= new Server();
+        ClientHandler clientHandler1= new ClientHandler(server);
+        ClientController clientController= new ClientController(server,clientHandler1) ;
+
+        ClientHandler clientHandler2= new ClientHandler(server);
+        ClientController clientController2= new ClientController(server,clientHandler2) ;
+
+        ClientHandler clientHandler3= new ClientHandler(server);
+        ClientController clientController3= new ClientController(server,clientHandler3) ;
+
+        ClientHandler clientHandler4= new ClientHandler(server);
+        ClientController clientController4= new ClientController(server,clientHandler4) ;
+
+        clientControllers.add(clientController);
+        clientControllers.add(clientController2);
+        clientControllers.add(clientController3);
+        clientControllers.add(clientController4);
+
+        clientController.setNickname("simo");
+        clientController2.setNickname("ali");
+        clientController3.setNickname("ale");
+        clientController4.setNickname("lillo");
+        GameMultiPlayer game = new GameMultiPlayer(4, nickname, true, clientControllers);
+        if(game.checkNickname("ali"))
+            game.askInfoOnPlayer(1,"ali");
+        else
+        {
+
+            game.disconnectPlayerOption("lillo");
+            game.disconnectPlayer("lillo");
+            assertTrue(game.numPlayersDisconnected()==1);
+        }
+
+        FileClass.FileDestroyer();
+
+    }
+
+
+
+
 
 }
